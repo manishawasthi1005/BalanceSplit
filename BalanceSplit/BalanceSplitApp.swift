16cfc852561a5e405f19db06bc0d2e67265fa323
Manish <a.manish1005@gmail.com>
2023-05-21 09:43:26 +0200

    Initial Commit

---
+++ BalanceSplit/BalanceSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceSplitApp.swift
+//  BalanceSplit
+//
+//  Created by manish.m.awasthi on 21.05.23.
+//
+
+import SwiftUI
+
+@main
+struct BalanceSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
